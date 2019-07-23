1e8f9bc1d108e5cb929cd4237b9816bd219abec3
yangchigi <yangchigi@yangchigi.com>
2019-07-23 15:00:52 +0900

    yng

---
+++ conftest.cpp
@@ -0,0 +1,33 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Yangcoin Core"
+#define PACKAGE_TARNAME "yangcoin"
+#define PACKAGE_VERSION "0.14.5"
+#define PACKAGE_STRING "Yangcoin Core 0.14.5"
+#define PACKAGE_BUGREPORT "https://github.com/yangcoin/yangcoin/issues"
+#define PACKAGE_URL "http://www.yangcoin.org/"
+#define HAVE_CXX11 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+#define HAVE_PTHREAD 1
+#define HAVE_DECL_STRERROR_R 1
+#define HAVE_STRERROR_R 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_SELECT_H 1
+/* end confdefs.h.  */
+#include <sys/prctl.h>
